37c31f15e508bbc7ab0ba6ea685d94bae17c63f5
Tezla-Lee <tezla.lee@gmail.com>
2021-04-27 15:27:22 +0900

    conf: Init Project
    - yml
    - etc

---
+++ src/main/java/com/usbridge/bongdari/BongdariApplication.java
@@ -0,0 +1,13 @@
+package com.usbridge.bongdari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongdariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BongdariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usbridge/bongdari/BongdariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usbridge.bongdari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongdariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
