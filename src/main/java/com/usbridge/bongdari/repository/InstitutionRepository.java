e13eb70de73c12aa74df5a0affee3862dd431560
Tezla-Lee <tezla.lee@gmail.com>
2021-05-04 22:51:17 +0900

    feat: Create InstitutionRepository
    Create InstitutionRepository

---
+++ src/main/java/com/usbridge/bongdari/repository/InstitutionRepository.java
@@ -0,0 +1,8 @@
+package com.usbridge.bongdari.repository;
+
+import com.usbridge.bongdari.model.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+
+}
