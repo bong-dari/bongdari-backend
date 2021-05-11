a49982c4fbcba6482214f9fb702dc41739423ea3
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-11 15:13:16 +0900

    feat: Update VolunteerController
    - getVolunteer 메소드 추가

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -1,7 +1,15 @@
 package com.usbridge.bongdari.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class VolunteerController {
+
+    @GetMapping("/volunteer/{id}")
+    public void getVolunteer(@PathVariable Long id) {
+    }
 }
