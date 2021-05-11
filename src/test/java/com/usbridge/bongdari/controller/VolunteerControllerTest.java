5785d16bae7ef77ebc70d357f2f640b67fb661e0
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-11 14:40:17 +0900

    feat: Create VolunteerControllerTest

---
+++ src/test/java/com/usbridge/bongdari/controller/VolunteerControllerTest.java
@@ -0,0 +1,10 @@
+package com.usbridge.bongdari.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class VolunteerControllerTest {
+
+}
