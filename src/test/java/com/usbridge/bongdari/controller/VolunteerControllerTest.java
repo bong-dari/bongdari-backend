7f86fed233f45c22a2c760ce5a5f8f40909b46bf
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-11 14:47:56 +0900

    feat: Update VolunteerControllerTest
    - 봉사공고가져오기_성공 테스트 메소드 추가

--- src/test/java/com/usbridge/bongdari/controller/VolunteerControllerTest.java
+++ src/test/java/com/usbridge/bongdari/controller/VolunteerControllerTest.java
@@ -1,10 +1,22 @@
 package com.usbridge.bongdari.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class VolunteerControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void 봉사공고가져오기_성공() throws Exception {
+        mockMvc.perform(get("/api/volunteer/1"))
+                .andExpect(status().isOk());
+    }
 }
