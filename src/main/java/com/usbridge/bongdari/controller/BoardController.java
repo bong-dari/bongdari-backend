c1b0c6543b17b7496eb64cd22d10d03b2c4423bc
LNoah <limnoah0301@gmail.com>
2021-05-11 23:11:40 +0900

    feat: Add getBoard API to BoardController.
    Add getBoard API.

--- src/main/java/com/usbridge/bongdari/controller/BoardController.java
+++ src/main/java/com/usbridge/bongdari/controller/BoardController.java
@@ -1,4 +1,18 @@
 package com.usbridge.bongdari.controller;
 
+import com.usbridge.bongdari.controller.dto.BoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class BoardController {
+
+    @GetMapping("/board/{board_id}")
+    public BoardDto getBoard(@PathVariable Long board_id){
+        return null;
+    }
+
 }
