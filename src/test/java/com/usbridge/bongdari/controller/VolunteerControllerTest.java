01e2f42a75bd436ee5e2043e0b79bfa6e0305521
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-12 11:32:11 +0900

    test: Update VolunteerControllerTest
    - Add 봉사공고등록_불필요한입력값()
    - WebMvcTest -> SpringBootTest

--- src/test/java/com/usbridge/bongdari/controller/VolunteerControllerTest.java
+++ src/test/java/com/usbridge/bongdari/controller/VolunteerControllerTest.java
@@ -1,14 +1,14 @@
 package com.usbridge.bongdari.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.usbridge.bongdari.controller.dto.VolunteerDto;
 import com.usbridge.bongdari.model.Volunteer;
 import com.usbridge.bongdari.model.enums.Gender;
-import com.usbridge.bongdari.repository.VolunteerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,14 +18,13 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.time.LocalDate;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class VolunteerControllerTest {
 
     @Autowired
@@ -33,9 +32,6 @@ class VolunteerControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @MockBean
-    VolunteerRepository volunteerRepository;
 
     @BeforeEach
     void before(WebApplicationContext was) {
@@ -47,6 +43,29 @@ class VolunteerControllerTest {
 
     @Test
     public void 봉사공고등록() throws Exception {
+        VolunteerDto volunteerDto = VolunteerDto.builder()
+                .title("봉사 활동")
+                .details("봉사하기")
+                .time("오전10시부터")
+                .contact("010-1234-5678")
+                .capacity(10)
+                .gender(Gender.ALL)
+                .location("마포구")
+                .manager("이재복")
+                .startDate(LocalDate.of(2050, 5, 1))
+                .endDate(LocalDate.of(2020, 5, 10))
+                .build();
+
+        mockMvc.perform(post("/api/volunteer")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(volunteerDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").exists());
+    }
+
+    @Test
+    public void 봉사공고등록_불필요한입력값() throws Exception {
         Volunteer volunteer = Volunteer.builder()
                 .id(50L)
                 .title("봉사 활동")
@@ -59,28 +78,13 @@ class VolunteerControllerTest {
                 .manager("이재복")
                 .startDate(LocalDate.of(2050, 5, 1))
                 .endDate(LocalDate.of(2020, 5, 10))
-                .createdDate(LocalDate.now())
+                .createdDate(LocalDate.of(2020, 5, 10))
                 .build();
-
-        when(volunteerRepository.save(volunteer)).thenReturn(volunteer);
 
         mockMvc.perform(post("/api/volunteer")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(volunteer)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("id").exists());
-    }
-
-    @Test
-    public void 봉사공고가져오기_성공() throws Exception {
-        mockMvc.perform(get("/api/volunteer/1"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void 봉사공고가져오기_실패() throws Exception {
-        mockMvc.perform(get("/api/volunteer/10000"))
                 .andExpect(status().isBadRequest());
     }
 }
