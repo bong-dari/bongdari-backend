ad5c5eb3dfd808b552205ab304868efb321eed60
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-12 11:33:19 +0900

    feat: Update VolunteerController
    - Add ModelMapper

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -1,8 +1,10 @@
 package com.usbridge.bongdari.controller;
 
+import com.usbridge.bongdari.controller.dto.VolunteerDto;
 import com.usbridge.bongdari.model.Volunteer;
 import com.usbridge.bongdari.repository.VolunteerRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,12 @@ public class VolunteerController {
 
     private final VolunteerRepository volunteerRepository;
 
+    private final ModelMapper modelMapper;
+
     @PostMapping("/volunteer")
-    public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer) {
+    public ResponseEntity<Volunteer> createVolunteer(@RequestBody VolunteerDto volunteerDto) {
+        Volunteer volunteer = modelMapper.map(volunteerDto, Volunteer.class);
+
         Volunteer newVolunteer = volunteerRepository.save(volunteer);
 
         return ResponseEntity.ok().body(newVolunteer);
