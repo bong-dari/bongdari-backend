694825c076b391644e65eb2bc96b698d42e1f0bd
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-12 09:47:28 +0900

    feat: Update VolunteerController
    Add createVolunteer
    POST "api/volunteer"

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -1,15 +1,24 @@
 package com.usbridge.bongdari.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.usbridge.bongdari.model.Volunteer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/api")
 public class VolunteerController {
 
-    @GetMapping("/volunteer/{id}")
-    public void getVolunteer(@PathVariable Long id) {
+    @PostMapping("/volunteer")
+    public ResponseEntity createVolunteer() {
+
+        URI createdUri = linkTo(Volunteer.class).slash("{id}").toUri();
+
+        return ResponseEntity.created(createdUri).build();
     }
 }
