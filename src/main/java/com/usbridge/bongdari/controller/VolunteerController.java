8619046706426237a00a07234c80162fa244ae61
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-12 10:20:35 +0900

    refactor: Update VolunteerController
    - Add dependency (VolunteerRepository)
    - Change response status code: 201 -> 200

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -1,24 +1,25 @@
 package com.usbridge.bongdari.controller;
 
 import com.usbridge.bongdari.model.Volunteer;
+import com.usbridge.bongdari.repository.VolunteerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api")
 public class VolunteerController {
 
+    private final VolunteerRepository volunteerRepository;
+
     @PostMapping("/volunteer")
-    public ResponseEntity createVolunteer() {
+    public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer) {
+        Volunteer newVolunteer = volunteerRepository.save(volunteer);
 
-        URI createdUri = linkTo(Volunteer.class).slash("{id}").toUri();
-
-        return ResponseEntity.created(createdUri).build();
+        return ResponseEntity.ok().body(newVolunteer);
     }
 }
