9d03b02b78e3c88a43db6496cb331c48dd107e1c
LNoah <limnoah0301@gmail.com>
2021-05-14 16:28:41 +0900

    refactor: Update getBoard API.
    Add service method to return object.

--- src/main/java/com/usbridge/bongdari/controller/BoardController.java
+++ src/main/java/com/usbridge/bongdari/controller/BoardController.java
@@ -1,18 +1,21 @@
 package com.usbridge.bongdari.controller;
 
-import com.usbridge.bongdari.controller.dto.BoardDto;
+import com.usbridge.bongdari.controller.dto.BoardResponseDto;
+import com.usbridge.bongdari.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RequiredArgsConstructor
-@RestController
 @RequestMapping("/api")
+@RestController
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping("/board/{board_id}")
-    public BoardDto getBoard(@PathVariable Long board_id){
-        return null;
+    public BoardResponseDto getBoard(@PathVariable Long board_id){
+        return boardService.getBoard(board_id);
     }
 
 }
