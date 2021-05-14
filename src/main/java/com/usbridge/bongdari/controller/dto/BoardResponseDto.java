f28e667a719e67c277bc2142c8042493f41f4911
LNoah <limnoah0301@gmail.com>
2021-05-14 16:40:38 +0900

    refactor: Update BoardResponseDto.
    Add Builder annotation.

--- src/main/java/com/usbridge/bongdari/controller/dto/BoardResponseDto.java
+++ src/main/java/com/usbridge/bongdari/controller/dto/BoardResponseDto.java
@@ -1,21 +1,23 @@
 package com.usbridge.bongdari.controller.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class BoardDto {
+public class BoardResponseDto {
     private String nickname;
     private String contact;
     private LocalDate startDate;
     private LocalDate endDate;
-    private int capacity;
     private LocalDate createdDate;
+    private int capacity;
     private String city;
     private String gu;
 }
