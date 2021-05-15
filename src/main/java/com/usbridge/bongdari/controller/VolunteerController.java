2a26b01d8c43609835acad83cbe41cd4334fc152
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-15 11:42:26 +0900

    feat: Update VolunteerController
    - Add Errors (Validation 에서 발생한 에러를 받음)
    - 에러 발생 시 Bad Request

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -6,10 +6,13 @@ import com.usbridge.bongdari.repository.VolunteerRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,7 +24,11 @@ public class VolunteerController {
     private final ModelMapper modelMapper;
 
     @PostMapping("/volunteer")
-    public ResponseEntity<Volunteer> createVolunteer(@RequestBody VolunteerDto volunteerDto) {
+    public ResponseEntity<Volunteer> createVolunteer(@RequestBody @Valid VolunteerDto volunteerDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
         Volunteer volunteer = modelMapper.map(volunteerDto, Volunteer.class);
 
         Volunteer newVolunteer = volunteerRepository.save(volunteer);
