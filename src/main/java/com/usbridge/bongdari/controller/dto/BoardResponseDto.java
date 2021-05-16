ac23c048897fc6b2f44ae8fba6d3f58672c7880e
LNoah <limnoah0301@gmail.com>
2021-05-17 03:54:32 +0900

    refactor: Update BoardResponseDto's constructor.
    Add constructor for use to change Board Entity to BoardResponseDto.

--- src/main/java/com/usbridge/bongdari/controller/dto/BoardResponseDto.java
+++ src/main/java/com/usbridge/bongdari/controller/dto/BoardResponseDto.java
@@ -1,11 +1,14 @@
 package com.usbridge.bongdari.controller.dto;
 
+import com.usbridge.bongdari.model.Board;
+import com.usbridge.bongdari.model.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -14,10 +17,25 @@ import java.time.LocalDate;
 public class BoardResponseDto {
     private String nickname;
     private String contact;
+    private Category category;
+    private String details;
     private LocalDate startDate;
     private LocalDate endDate;
-    private LocalDate createdDate;
+    private LocalDateTime createdDate;
     private int capacity;
     private String city;
     private String gu;
+
+    public BoardResponseDto(Board board) {
+        this.nickname = board.getMember().getNickname();
+        this.contact = board.getContact();
+        this.category = board.getCategory();
+        this.details = board.getDetails();
+        this.startDate = board.getStartDate();
+        this.endDate = board.getEndDate();
+        this.createdDate = board.getCreatedDate();
+        this.capacity = board.getCapacity();
+        this.city = board.getCity();
+        this.gu = board.getGu();
+    }
 }
