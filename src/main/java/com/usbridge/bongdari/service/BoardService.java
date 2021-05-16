9baf4f803d66ee00aeadacac51527af602170766
LNoah <limnoah0301@gmail.com>
2021-05-16 17:27:56 +0900

    refactor: Refactor BoardService.
    Refactor BoardService's methods.
    
    - findByCategory
    	- Change return type.
    	- Refactor logic.
    - boardPageToBoardResponseDtoPage
    	- Change method name.
    - boardToBoardResponseDto
    	- Change method name.

--- src/main/java/com/usbridge/bongdari/service/BoardService.java
+++ src/main/java/com/usbridge/bongdari/service/BoardService.java
@@ -1,31 +1,57 @@
 package com.usbridge.bongdari.service;
 
 import com.usbridge.bongdari.controller.dto.BoardResponseDto;
-import com.usbridge.bongdari.exception.ResourceNotFoundException;
 import com.usbridge.bongdari.model.Board;
-import com.usbridge.bongdari.model.Member;
+import com.usbridge.bongdari.model.enums.Category;
 import com.usbridge.bongdari.repository.BoardRepository;
-import com.usbridge.bongdari.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
-    private final MemberRepository memberRepository;
 
-    public BoardResponseDto findById(Long id){
+    public Page<BoardResponseDto> findByCategory(int category_id, String city, String gu, Pageable pageable){
+        Page<Board> boardPage = gu == null ?
+                boardRepository.findByCategoryAndCity(Category.values()[category_id], city, pageable) :
+                boardRepository.findByCategoryAndCityAndGu(Category.values()[category_id], city, gu, pageable);
 
-        Board board = boardRepository.findById(id).orElseThrow(() ->
-                new ResourceNotFoundException("해당 게시글이 없습니다. id = " + id));
-        Member member = memberRepository.findById(board.getId()).orElseThrow(() ->
-                new ResourceNotFoundException("회원을 찾을 수 없습니다. id = " + board.getId()));
+        return boardPageToBoardResponseDtoPage(boardPage, pageable);
+    }
 
+//    @Transactional
+//    public Long saveBoard(BoardSaveRequestDto requestDto){
+////        return boardRepository.save(requestDto.toEntity()).getId();
+//        return null;
+//    }
+
+    private Page<BoardResponseDto> boardPageToBoardResponseDtoPage(Page<Board> boardPage, Pageable pageable){
+        List<Board> boardList = boardPage.toList();
+        List<BoardResponseDto> dtoList = new ArrayList<>();
+
+        for (Board board : boardList) {
+            dtoList.add(boardToBoardResponseDto(board));
+        }
+
+        int start = (int)pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), dtoList.size());
+        return new PageImpl<>(dtoList.subList(start, end), pageable, dtoList.size());
+    }
+
+    private BoardResponseDto boardToBoardResponseDto(Board board){
         return BoardResponseDto.builder()
-                .nickname(member.getNickname())
+                .nickname(board.getMember().getNickname())
                 .contact(board.getContact())
+                .category(board.getCategory())
+                .details(board.getDetails())
                 .startDate(board.getStartDate())
                 .endDate(board.getEndDate())
                 .createdDate(board.getCreatedDate())
