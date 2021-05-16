8fdcc0b791e86c07426af30e83d86abb5b72d591
LNoah <limnoah0301@gmail.com>
2021-05-16 17:25:03 +0900

    refactor: Refactor BoardController.
    Refactor getBoardList API's parameter, name, return type.

--- src/main/java/com/usbridge/bongdari/controller/BoardController.java
+++ src/main/java/com/usbridge/bongdari/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.usbridge.bongdari.controller;
 import com.usbridge.bongdari.controller.dto.BoardResponseDto;
 import com.usbridge.bongdari.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -13,9 +16,9 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("/board/{board_id}")
-    public BoardResponseDto getBoard(@PathVariable Long board_id){
-        return boardService.findById(board_id);
+    @GetMapping("/board/{category_id}")
+    public Page<BoardResponseDto> getBoardList(@PathVariable int category_id, @RequestParam(defaultValue = "서울시", required = false) String city,
+                                               @RequestParam(required = false) String gu, @PageableDefault(size = 10) Pageable pageable){
+        return boardService.findByCategory(category_id, city, gu, pageable);
     }
-
 }
