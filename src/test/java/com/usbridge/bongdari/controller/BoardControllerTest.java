7c36780bf4c50b6c75813dfa1a62945c3cada7a3
LNoah <limnoah0301@gmail.com>
2021-05-20 02:16:44 +0900

    test: Add BoardControllerTest.
    - Use SpringBootTest annotation
    - getBoardListNoCityAndGu test
    	- URL에 주소 파라미터가 없을 때 실행되는 테스트.
    - getBoardList test
    	- URL에 시, 구 주소 파라미터 전부 있을 때 테스트.

--- src/test/java/com/usbridge/bongdari/controller/BoardControllerTest.java
+++ src/test/java/com/usbridge/bongdari/controller/BoardControllerTest.java
@@ -7,12 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @SpringBootTest
 public class BoardControllerTest {
 
@@ -30,34 +33,18 @@ public class BoardControllerTest {
     @Test
     void getBoardListNoCityAndGu() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/board/2"))
-                .andExpect(status().isOk());
-//                .andExpect(jsonPath("$.nickname").value("jwljwl"))
-//                .andExpect(jsonPath("$.contact").value("01066075331"))
-//                .andExpect(jsonPath("$.category").value("TOWN"))
-//                .andExpect(jsonPath("$.details").value("봉사활동 게시판 테스트 입니다."))
-//                .andExpect(jsonPath("$.startDate").value("2021-05-20"))
-//                .andExpect(jsonPath("$.endDate").value("2021-05-30"))
-//                .andExpect(jsonPath("$.createdDate").value("2021-05-14T20:00:58"))
-//                .andExpect(jsonPath("$.capacity").value(3))
-//                .andExpect(jsonPath("$.city").value("서울시"))
-//                .andExpect(jsonPath("$.gu").value("강동구"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].category").value("TOWN"))
+                .andExpect(jsonPath("$.content[0].city").value("서울시"));
     }
 
     @DisplayName("게시판 조회")
     @Test
     void getBoardList() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/board/2?city=서울시&gu=강동구"))
-                .andExpect(status().isOk());
-//                .andExpect(jsonPath("$.nickname").value("jwljwl"))
-//                .andExpect(jsonPath("$.contact").value("01066075331"))
-//                .andExpect(jsonPath("$.category").value("TOWN"))
-//                .andExpect(jsonPath("$.details").value("봉사활동 게시판 테스트 입니다."))
-//                .andExpect(jsonPath("$.startDate").value("2021-05-20"))
-//                .andExpect(jsonPath("$.endDate").value("2021-05-30"))
-//                .andExpect(jsonPath("$.createdDate").value("2021-05-14T20:00:58"))
-//                .andExpect(jsonPath("$.capacity").value(3))
-//                .andExpect(jsonPath("$.city").value("서울시"))
-//                .andExpect(jsonPath("$.gu").value("강동구"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].category").value("TOWN"))
+                .andExpect(jsonPath("$.content[0].city").value("서울시"))
+                .andExpect(jsonPath("$.content[0].gu").value("강동구"));
     }
-
 }
