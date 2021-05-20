e3f0104b441b92674e931ca20b41cd6067427c37
LNoah <limnoah0301@gmail.com>
2021-05-21 02:03:48 +0900

    refactor: Refactor BoardRepositoryTest.

--- src/test/java/com/usbridge/bongdari/repository/BoardRepositoryTest.java
+++ src/test/java/com/usbridge/bongdari/repository/BoardRepositoryTest.java
@@ -5,7 +5,7 @@ import com.usbridge.bongdari.model.enums.Category;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
-//@DataJpaTest(properties = "spring.config.location=classpath:test.yml")
-@SpringBootTest
+@DataJpaTest
 public class BoardRepositoryTest {
 
     @Autowired
@@ -26,22 +25,22 @@ public class BoardRepositoryTest {
         Category category = Category.TOWN;
         String city = "서울시";
 
-        Pageable pageable = PageRequest.of(0, 1);
+        Pageable pageable = PageRequest.of(0, 5);
         Board board = boardRepository.findByCategoryAndCity(category, city, pageable).toList().get(0);
 
         assertThat(board.getCategory()).isEqualTo(Category.TOWN);
         assertThat(board.getCity()).isEqualTo(city);
     }
 
-    @DisplayName("카테고리, 도시별 게시글 검색")
+    @DisplayName("카테고리, 도시, 구별 게시글 검색")
     @Test
     void findByCategoryAndCityAndGu(){
         Category category = Category.TOWN;
         String city = "서울시";
         String gu = "강동구";
 
-        Pageable pageable = PageRequest.of(0, 1);
-        Board board = boardRepository.findByCategoryAndCity(category, city, pageable).toList().get(0);
+        Pageable pageable = PageRequest.of(0, 5);
+        Board board = boardRepository.findByCategoryAndCityAndGu(category, city, gu, pageable).toList().get(0);
 
         assertThat(board.getCategory()).isEqualTo(Category.TOWN);
         assertThat(board.getCity()).isEqualTo(city);
