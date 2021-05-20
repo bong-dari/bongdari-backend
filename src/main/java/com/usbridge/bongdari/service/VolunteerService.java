4ab3045d54b88803ffb6885ebcd1a4c8060a7bee
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-20 17:55:00 +0900

    feat: Update VolunteerService
    - Add createVolunteer
        - 컨트롤러 로직 서비스로 분리

--- src/main/java/com/usbridge/bongdari/service/VolunteerService.java
+++ src/main/java/com/usbridge/bongdari/service/VolunteerService.java
@@ -1,7 +1,23 @@
 package com.usbridge.bongdari.service;
 
+import com.usbridge.bongdari.controller.dto.VolunteerDto;
+import com.usbridge.bongdari.model.Volunteer;
+import com.usbridge.bongdari.repository.VolunteerRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class VolunteerService {
+
+    private final VolunteerRepository volunteerRepository;
+
+    private final ModelMapper modelMapper;
+
+    public Volunteer createVolunteer(VolunteerDto volunteerDto) {
+        Volunteer volunteer = modelMapper.map(volunteerDto, Volunteer.class);
+
+        return volunteerRepository.save(volunteer);
+    }
 }
