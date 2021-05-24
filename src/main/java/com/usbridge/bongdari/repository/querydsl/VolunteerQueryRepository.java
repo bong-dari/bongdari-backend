6b38bf72385510003ff77e150d8c4fa53df005f9
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-24 23:03:54 +0900

    feat: Update VolunteerQueryRepository
    - Add eqCity
    - Add eqGu

--- src/main/java/com/usbridge/bongdari/repository/querydsl/VolunteerQueryRepository.java
+++ src/main/java/com/usbridge/bongdari/repository/querydsl/VolunteerQueryRepository.java
@@ -1,11 +1,28 @@
 package com.usbridge.bongdari.repository.querydsl;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static com.usbridge.bongdari.model.QVolunteer.volunteer;
 
 @RequiredArgsConstructor
 @Repository
 public class VolunteerQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
+
+    private BooleanExpression eqCity(String city) {
+        if (city == null) {
+            return null;
+        }
+        return volunteer.city.eq(city);
+    }
+
+    private BooleanExpression eqGu(String gu) {
+        if (gu == null) {
+            return null;
+        }
+        return volunteer.gu.eq(gu);
+    }
 }
