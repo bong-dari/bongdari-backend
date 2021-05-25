8f5b18eb7640f524c26f5c2784d12992956009c3
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-25 23:56:36 +0900

    feat: Update VolunteerQueryRepositoryTest
    봉사활동 공고 가져오기 (기본) 추가

--- src/test/java/com/usbridge/bongdari/repository/querydsl/VolunteerQueryRepositoryTest.java
+++ src/test/java/com/usbridge/bongdari/repository/querydsl/VolunteerQueryRepositoryTest.java
@@ -1,11 +1,29 @@
 package com.usbridge.bongdari.repository.querydsl;
 
+import com.usbridge.bongdari.model.Volunteer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class VolunteerQueryRepositoryTest {
 
     @Autowired
     private VolunteerQueryRepository volunteerQueryRepository;
+
+    @Test
+    @DisplayName("봉사활동 공고 가져오기 (기본)")
+    public void getVolunteersBySearch() {
+        Page<Volunteer> volunteers = volunteerQueryRepository.getVolunteersBySearch(null, null, null,
+                null, null, PageRequest.of(0, 20));
+
+        assertThat(volunteers.getTotalElements()).isEqualTo(3);
+    }
 }
