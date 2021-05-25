e5823838b8d729a022378d1c54394c3950fdcc27
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-25 23:55:54 +0900

    feat: Create VolunteerQueryRepositoryTest
    init

---
+++ src/test/java/com/usbridge/bongdari/repository/querydsl/VolunteerQueryRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.usbridge.bongdari.repository.querydsl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerQueryRepositoryTest {
+
+    @Autowired
+    private VolunteerQueryRepository volunteerQueryRepository;
+}
