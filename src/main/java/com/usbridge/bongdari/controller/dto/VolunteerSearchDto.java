22ece4da3109df559ca2d41df64db891318d4de6
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-26 21:54:20 +0900

    feat: Create VolunteerSearchDto
    검색결과용 봉사 공고 DTO

---
+++ src/main/java/com/usbridge/bongdari/controller/dto/VolunteerSearchDto.java
@@ -0,0 +1,19 @@
+package com.usbridge.bongdari.controller.dto;
+
+import java.time.LocalDate;
+
+public class VolunteerSearchDto {
+    private long id;
+
+    private String title;
+
+    private String city;
+
+    private String gu;
+
+    private Integer capacity;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+}
