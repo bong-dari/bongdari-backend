4fe360315535a1d154e2deaf21ff713c058af2f9
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-27 00:18:58 +0900

    feat: Update VolunteerController
    getVolunteersBySearch 추가 (검색 API)

--- src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
+++ src/main/java/com/usbridge/bongdari/controller/VolunteerController.java
@@ -1,12 +1,16 @@
 package com.usbridge.bongdari.controller;
 
 import com.usbridge.bongdari.controller.dto.VolunteerDto;
+import com.usbridge.bongdari.controller.dto.VolunteerSearchDto;
+import com.usbridge.bongdari.exception.BadRequestException;
 import com.usbridge.bongdari.model.Volunteer;
 import com.usbridge.bongdari.service.VolunteerService;
-import exception.BadRequestException;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,9 +19,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -38,10 +46,28 @@ public class VolunteerController {
     }
 
     @GetMapping("/volunteer/{id}")
-    public ResponseEntity<VolunteerDto> getVolunteer(@PathVariable Long id) throws NotFoundException {
+    public ResponseEntity<VolunteerDto> getVolunteer(@PathVariable Long id) {
         Volunteer volunteer = volunteerService.findVolunteerById(id);
 
         return ResponseEntity.ok(modelMapper.map(volunteer, VolunteerDto.class));
+    }
+
+    @GetMapping("/volunteers")
+    public ResponseEntity<Page<VolunteerSearchDto>> getVolunteersBySearch(@RequestParam(required = false) LocalDate date,
+                                                                          @RequestParam(required = false) Integer capacity,
+                                                                          @RequestParam(required = false) String city,
+                                                                          @RequestParam(required = false) String gu,
+                                                                          @RequestParam(required = false) String searchKeyword,
+                                                                          @PageableDefault(size = 20) Pageable page) {
+
+        Page<Volunteer> volunteers = volunteerService.findVolunteersBySearch(date, capacity, city, gu, searchKeyword, page);
+
+        List<VolunteerSearchDto> volunteerSearchDtos = volunteers
+                .stream()
+                .map(volunteer -> modelMapper.map(volunteer, VolunteerSearchDto.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new PageImpl<>(volunteerSearchDtos, page, volunteers.getTotalElements()));
     }
 
     @DeleteMapping("/volunteer/{id}")
