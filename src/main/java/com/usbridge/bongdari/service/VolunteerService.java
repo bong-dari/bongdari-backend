c9813e6c115e3564797a3df872442f3a31bb8830
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-27 00:20:01 +0900

    feat: Update VolunteerService
    - findVolunteerById 변경
        - Exception 변경
    - findVolunteersBySearch 추가

--- src/main/java/com/usbridge/bongdari/service/VolunteerService.java
+++ src/main/java/com/usbridge/bongdari/service/VolunteerService.java
@@ -1,22 +1,18 @@
 package com.usbridge.bongdari.service;
 
 import com.usbridge.bongdari.controller.dto.VolunteerDto;
-import com.usbridge.bongdari.controller.dto.VolunteerSearchDto;
+import com.usbridge.bongdari.exception.BadRequestException;
+import com.usbridge.bongdari.exception.ResourceNotFoundException;
 import com.usbridge.bongdari.model.Volunteer;
 import com.usbridge.bongdari.repository.VolunteerRepository;
 import com.usbridge.bongdari.repository.querydsl.VolunteerQueryRepository;
-import exception.BadRequestException;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,8 +30,8 @@ public class VolunteerService {
         return volunteerRepository.save(volunteer);
     }
 
-    public Volunteer findVolunteerById(Long id) throws NotFoundException {
-        return volunteerRepository.findById(id).orElseThrow(() -> new NotFoundException("존재하지 않는 봉사활동 공고입니다."));
+    public Volunteer findVolunteerById(Long id) throws ResourceNotFoundException {
+        return volunteerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 봉사활동 공고입니다."));
     }
 
     public Volunteer deleteVolunteerById(Long id) throws BadRequestException {
@@ -46,15 +42,7 @@ public class VolunteerService {
         return volunteer;
     }
 
-    public Page<VolunteerSearchDto> findVolunteersBySearch(LocalDate date, Integer capacity, String city, String gu, String searchKeyword, Pageable page) {
-
-        Page<Volunteer> volunteers = volunteerQueryRepository.getVolunteersBySearch(date, capacity, city, gu, searchKeyword, page);
-
-        List<VolunteerSearchDto> volunteerSearchDtos = volunteers
-                .stream()
-                .map(volunteer -> modelMapper.map(volunteer, VolunteerSearchDto.class))
-                .collect(Collectors.toList());
-
-        return new PageImpl<>(volunteerSearchDtos, page, volunteers.getTotalElements());
+    public Page<Volunteer> findVolunteersBySearch(LocalDate date, Integer capacity, String city, String gu, String searchKeyword, Pageable page) {
+        return volunteerQueryRepository.getVolunteersBySearch(date, capacity, city, gu, searchKeyword, page);
     }
 }
