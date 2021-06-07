83e36de6011000951087efdd7a637795d9604f22
LNoah <limnoah0301@gmail.com>
2021-06-07 16:54:58 +0900

    refactor: Update MemberRepository.java
    Add query method
    - findByEmail

--- src/main/java/com/usbridge/bongdari/repository/MemberRepository.java
+++ src/main/java/com/usbridge/bongdari/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.usbridge.bongdari.repository;
 import com.usbridge.bongdari.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
