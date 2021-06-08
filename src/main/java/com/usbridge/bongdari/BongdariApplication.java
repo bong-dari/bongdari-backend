9123ca906d8126f22aa7a1b829516ac245b1f501
LNoah <limnoah0301@gmail.com>
2021-06-09 01:14:23 +0900

    feat: Update BongdariApplication.java

--- src/main/java/com/usbridge/bongdari/BongdariApplication.java
+++ src/main/java/com/usbridge/bongdari/BongdariApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BongdariApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BongdariApplication.class, args);
     }
-
 }
