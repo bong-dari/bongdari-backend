39e4d01d9c82c4e61c58e671f4b32444c5990605
LNoah <limnoah0301@gmail.com>
2021-06-10 20:32:46 +0900

    refactor: Update MemberRepository.java
    Changed query method's name
    - findByEmail => findByEmailAndSns

--- src/main/java/com/usbridge/bongdari/repository/MemberRepository.java
+++ src/main/java/com/usbridge/bongdari/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package com.usbridge.bongdari.repository;
 
 import com.usbridge.bongdari.model.Member;
+import com.usbridge.bongdari.model.enums.SNS;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+    Optional<Member> findByEmailAndSns(String email, SNS sns);
 }
