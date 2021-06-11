b7c24ae2e977ea98f7572beb354de141ad16e4ce
Jaebok Lee <Tezla.lee@gmail.com>
2021-06-11 20:42:32 +0900

    feat: Update VolunteerService
    Create modifyVolunteer

--- src/main/java/com/usbridge/bongdari/service/VolunteerService.java
+++ src/main/java/com/usbridge/bongdari/service/VolunteerService.java
@@ -11,6 +11,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -44,5 +45,19 @@ public class VolunteerService {
 
     public Page<Volunteer> findVolunteersBySearch(LocalDate date, Integer capacity, String city, String gu, String searchKeyword, Pageable page) {
         return volunteerQueryRepository.getVolunteersBySearch(date, capacity, city, gu, searchKeyword, page);
+    }
+
+    @Transactional
+    public Volunteer modifyVolunteer(VolunteerDto volunteerDto) {
+        if (volunteerDto.getId() == null) {
+            throw new BadRequestException("봉사활동 공고 id 값이 null 입니다.");
+        }
+
+        Volunteer volunteer = volunteerRepository.findById(volunteerDto.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 봉사활동 공고입니다."));
+
+        modelMapper.map(volunteerDto, volunteer);
+
+        return volunteer;
     }
 }
