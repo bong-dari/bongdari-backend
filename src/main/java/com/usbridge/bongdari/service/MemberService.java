338f2115e23da7b34f00ffee6ec9dd9bf2486aa7
LNoah <limnoah0301@gmail.com>
2021-06-22 02:30:40 +0900

    feat: Update MemberService.java
    Update updateMember method.

--- src/main/java/com/usbridge/bongdari/service/MemberService.java
+++ src/main/java/com/usbridge/bongdari/service/MemberService.java
@@ -15,13 +15,11 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Member update(Long id, MemberDto requestDto){
+    public Member updateMember(Long id, MemberDto requestDto){
         Member member = memberRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 사용자가 없습니다. id = " + id));
-        System.out.println("<requestDto>");
-        System.out.println("nickname : " + requestDto.getNickname());
-        System.out.println("mobile : " + requestDto.getMobile());
-        System.out.println("smsAgreement : " + requestDto.getSmsAgreement());
+
         member.update(requestDto.getNickname(), requestDto.getMobile(), requestDto.getSmsAgreement());
+
         return memberRepository.save(member);
     }
 }
